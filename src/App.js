e830071fd2916f9ec2ceb1753ad9d6a267472d9a
Abhishek <abhishekmandha2018@gmail.com>
2023-07-29 01:52:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Todowrapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <Todowrapper/>
+    </div>
+  );
+}
+
+export default App;
